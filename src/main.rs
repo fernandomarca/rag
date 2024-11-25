649763b677261649ecd1e0c728b258b504779731
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2024-11-25 10:49:28 -0300

    rag

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use async_trait::async_trait;
 use futures::StreamExt;
 use langchain_rust::add_documents;
 use langchain_rust::chain::Chain;
@@ -16,46 +17,52 @@ use langchain_rust::prompt::HumanMessagePromptTemplate;
 use langchain_rust::prompt_args;
 use langchain_rust::schemas::Message;
 use langchain_rust::template_jinja2;
+use langchain_rust::text_splitter::TextSplitter;
+use langchain_rust::text_splitter::TextSplitterError;
+use langchain_rust::vectorstore::pgvector::Store;
 use langchain_rust::vectorstore::pgvector::StoreBuilder;
 use langchain_rust::vectorstore::Retriever;
 use langchain_rust::vectorstore::VecStoreOptions;
 use langchain_rust::vectorstore::VectorStore;
+use text_splitter::TextSplitter as Splitter;
+struct MyTextSplitter {}
 
-#[tokio::main]
-async fn main() {
-    // let path = "./src/fossa.pdf";
+#[async_trait]
+impl TextSplitter for MyTextSplitter {
+    async fn split_text(&self, text: &str) -> Result<Vec<String>, TextSplitterError> {
+        let result = Splitter::new(2000)
+            .chunks(text)
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
+        Ok(result)
+    }
+}
 
-    // let loader = PdfExtractLoader::from_path(path).expect("Failed to create PdfExtractLoader");
+async fn embedding(path: &str, store: Store) {
+    let ollama = OllamaEmbedder::default().with_model("mxbai-embed-large");
+    let loader = PdfExtractLoader::from_path(path).expect("Failed to create PdfExtractLoader");
 
-    // let documents = loader
-    //     .load()
-    //     .await
-    //     .unwrap()
-    //     .map(|d| d.unwrap())
-    //     .collect::<Vec<_>>()
-    //     .await;
+    let splitter = MyTextSplitter {};
 
-    let ollama = OllamaEmbedder::default().with_model("llama3.2");
-
-    let store = StoreBuilder::new()
-        .embedder(ollama)
-        .vstore_options(VecStoreOptions::default())
-        .collection_name("fossa")
-        .vector_dimensions(3072)
-        .connection_url("postgresql://postgres:123456@localhost:5432/postgres")
-        .build()
+    let documents = loader
+        .load_and_split(splitter)
         .await
-        .unwrap();
+        .unwrap()
+        .map(|d| d.unwrap())
+        .collect::<Vec<_>>()
+        .await;
 
-    // let _ = add_documents!(store, &documents).await.map_err(|e| {
-    //     println!("Error adding documents: {:?}", e);
-    // });
+    for doc in &documents {
+        let _ = add_documents!(store, &documents).await.map_err(|e| {
+            println!("Error adding documents: {:?}", e);
+        });
+    }
+}
 
+async fn ask(store: Store, question: &str) {
     let options = GenerationOptions::default().temperature(0.0).num_thread(8);
 
-    let llm = Ollama::default()
-        .with_model("llama3.2")
-        .with_options(options);
+    let llm = Ollama::default().with_model("llama3.2").with_options(options);
 
     let prompt= message_formatter![
                     fmt_message!(Message::new_system_message("Você é um assistente útil")),
@@ -76,13 +83,13 @@ async fn main() {
         .return_source_documents(true)
         .rephrase_question(true)
         .memory(SimpleMemory::new().into())
-        .retriever(Retriever::new(store, 3))
+        .retriever(Retriever::new(store, 10))
         .prompt(prompt)
         .build()
         .expect("Error building ConversationalChain");
 
     let input_variables = prompt_args! {
-        "question" => "Qual as principais recomendações para construir uma fossa?",
+        "question" => question
     };
 
     let mut stream = chain.stream(input_variables).await.unwrap();
@@ -94,4 +101,24 @@ async fn main() {
             }
         }
     }
+}
+
+#[tokio::main]
+async fn main() {
+    let ollama = OllamaEmbedder::default().with_model("mxbai-embed-large:latest");
+
+    let store = StoreBuilder::new()
+        .embedder(ollama)
+        .vstore_options(VecStoreOptions::default())
+        .vector_dimensions(1024)
+        .collection_name("pops")
+        .connection_url("postgresql://postgres:123456@localhost:5432/postgres")
+        .build()
+        .await
+        .unwrap();
+
+    // embedding("./src/pops.pdf", store).await;
+
+    let question = "procure por um documento que fale sobre mapeamento de pneus";
+    ask(store, question).await;
 }
